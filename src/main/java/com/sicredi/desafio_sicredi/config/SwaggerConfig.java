798a21a9eec904e43d53df10c43801951ebba0c1
Dalila Santos <seu-email@gmail.com>
2025-04-06 17:29:48 -0300

    chore: ajustes gerais de configuração e estrutura

--- src/main/java/com/sicredi/desafio_sicredi/config/SwaggerConfig.java
+++ src/main/java/com/sicredi/desafio_sicredi/config/SwaggerConfig.java
@@ -1,4 +1,19 @@
 package com.sicredi.desafio_sicredi.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Votação Sicredi")
+                        .version("1.0")
+                        .description("Documentação da API de votação com Spring Boot, Kafka e PostgreSQL.")
+                );
+    }
 }
